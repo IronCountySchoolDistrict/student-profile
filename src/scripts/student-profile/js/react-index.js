e09a922f0b5558ec17bdf9fc9efd1fd73162c6f1
Nathan Jones <purkinje90@gmail.com>
2016-03-17 11:09:48 -0600

    Use Promise.all() load PowerQuery results in new react version.

--- src/scripts/student-profile/js/react-index.js
+++ src/scripts/student-profile/js/react-index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ContactTable from './components/student-contacts/ContactTable';
+import General from './components/general/General';
 
-window.fetch('/ws/schema/query/com.icsd.sp.overview.contacts', {
+const contactsFetch = window.fetch('/ws/schema/query/com.icsd.sp.overview.contacts', {
     credentials: 'include',
     method: 'post',
     headers: {
@@ -15,16 +16,43 @@ window.fetch('/ws/schema/query/com.icsd.sp.overview.contacts', {
   })
   .then(r => r.json())
   .then(r => {
-  	// The `phones` key is surrounded in quotes, but is actually a JSON object,
-  	// so call JSON.parse on it to parse it
-  	return r.record.map(contact => {
-  		contact.phone = JSON.parse(contact.phone);
-  		return contact;
-  	});
+    // The `phones` key is surrounded in quotes, but is actually a JSON object,
+    // so call JSON.parse on it to parse it
+    return r.record.map(contact => {
+      contact.phone = JSON.parse(contact.phone);
+      return contact;
+    });
+  });
+
+const generalFetch = window.fetch('/ws/schema/query/com.icsd.sp.overview.general', {
+    credentials: 'include',
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      students_dcid: getParameterByName('frn').slice(3)
+    })
   })
-  .then(ajaxContactData => {
-	ReactDOM.render(
-		<ContactTable contacts={ajaxContactData} />, 
-		document.getElementById('student-contacts-container')
-	);
+  .then(r => r.json());
+
+Promise.all([
+    contactsFetch,
+    generalFetch
+  ])
+  .then(results => ({
+    contacts: results[0],
+    general: results[1]
+  }))
+  .then(results => {
+    ReactDOM.render(
+      <ContactTable contacts={results.contacts}/>,
+      document.getElementById('student-contacts-container')
+    );
+
+    ReactDOM.render(
+      <General general={results.general.record[0]}/>,
+      document.getElementById('general-container')
+    );
   });
