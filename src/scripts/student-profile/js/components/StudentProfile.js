5b0e578331c762d8702e290861663d46585d7bfc
Nathan Jones <nathan.jones@ironmail.org>
2016-08-09 08:26:26 -0600

    Created top-level react component for student profile

---
+++ src/scripts/student-profile/js/components/StudentProfile.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import ContactList from './student-contacts/ContactList';
+import General from './general/General';
+import CourseTable from './schedule/CourseTable';
+
+export default class StudentProfile extends Component {
+  render() {
+    return (
+      <div>
+        <div className="col-md-2" id="general-container">
+          <General general={this.props.results.general}/>
+        </div>
+        <div className="col-md-10">
+            <div id="student-contacts-container">
+              <ContactList contacts={this.props.results.contacts}/>
+            </div>
+            <div id="schedule-container">
+              <CourseTable courses={this.props.results.schedule} gpa={this.props.results.gpa}/>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
