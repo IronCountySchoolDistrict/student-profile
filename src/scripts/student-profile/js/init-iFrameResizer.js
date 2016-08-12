0f8f3a527e72d3784f79471004facaf218b8463f
Nathan Jones <nathan.jones@ironmail.org>
2016-08-12 14:30:42 -0600

    Use iFrameResizer to keep iFrame the right size

--- src/scripts/student-profile/js/init-iFrameResizer.js
+++ src/scripts/student-profile/js/init-iFrameResizer.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 import iFrameResizer from 'iFrameResizer';
 
-export default function() {
-  $('#profile-frame').iFrameResize();
+export function init() {
+  $('#profile-frame').iFrameResize({
+    log: false
+  });
 }
