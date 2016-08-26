ea5b75d0feb1a8444b8f487d743ba946c1d60875
Nathan Jones <nathan.jones@ironmail.org>
2016-08-26 16:33:19 -0600

    Use urijs to account for when student profile is not open in an iframe

--- src/scripts/student-profile/js/util.js
+++ src/scripts/student-profile/js/util.js
@@ -1,8 +1,16 @@
+import URI from 'urijs';
+
 export function getPortal() {
-  var path = window.top.location.pathname;
-  var pos = path.indexOf('/', 1);
+  let path = window.top.location.pathname;
+  const pos = path.indexOf('/', 1);
   if (pos > 0) {
     path = path.substring(1, pos);
   }
-  return path;
+  if (path !== 'teachers' && path !== 'admin') {
+    const lastHref = Cookies.get('lastHref');
+    const lastHrefUri = new URI(lastHref);
+    return lastHrefUri.segment()[0];
+  } else {
+    return path;
+  }
 }
