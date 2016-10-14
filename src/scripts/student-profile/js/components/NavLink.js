ca39704d859116e49a0e7582bdb4da0d1bdfef9b
Nathan Jones <nathan.jones@ironmail.org>
2016-10-14 13:48:24 -0600

    Use .withRouter() instead of context to get active link, since context API apparently isn't quite stable

--- src/scripts/student-profile/js/components/NavLink.js
+++ src/scripts/student-profile/js/components/NavLink.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import { Link, IndexLink } from 'react-router';
+import { Link, withRouter } from 'react-router';
 
-export default class NavLink extends Component {
-  constructor(props, context) {
-    super(props, context);
-  }
-
+const NavLink = class NavLink extends Component {
   render() {
-    const { router } = this.context;
-    const { index, to, children, ...props } = this.props;
+    const { to, children, ...props } = this.props;
 
-    const isActive = router.isActive(to);
+    const isActive = this.props.router.isActive(to);
 
     return (
       <li className={isActive ? 'active' : ''}>
@@ -18,8 +13,6 @@ export default class NavLink extends Component {
       </li>
     );
   }
-}
-
-NavLink.contextTypes = {
-  router: React.PropTypes.object
 };
+
+export default withRouter(NavLink);
