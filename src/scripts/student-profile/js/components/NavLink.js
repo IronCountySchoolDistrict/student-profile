125e9e55f001ff8189146e333ea4154f2044101c
Nathan Jones <nathan.jones@ironmail.org>
2016-10-21 12:20:23 -0600

    pass props to Link

--- src/scripts/student-profile/js/components/NavLink.js
+++ src/scripts/student-profile/js/components/NavLink.js
@@ -3,13 +3,13 @@ import { Link, withRouter } from 'react-router';
 
 const NavLink = class NavLink extends Component {
   render() {
-    const { to, children, ...props } = this.props;
+    const { to, children } = this.props;
 
     const isActive = this.props.router.isActive(to);
 
     return (
       <li className={isActive ? 'active' : ''}>
-        <Link to={to} {...props.route}>
+        <Link to={to} {...this.props.route}>
           {children}
         </Link>
       </li>
