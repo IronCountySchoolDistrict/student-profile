4031b5e78dc3113d9d3cb7c89fcf2edecaa225cc
Nathan Jones <nathan.jones@ironmail.org>
2016-10-21 14:42:36 -0600

    Moved data-source calls to each root component so they can be in charge of displaying the loading UI

--- src/scripts/student-profile/js/react-index.js
+++ src/scripts/student-profile/js/react-index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, IndexRoute, Route, hashHistory } from 'react-router';
+import URI from 'urijs';
+
 import Overview from './components/overview/Overview';
 import Nav from './components/Nav';
 import Schedule from './components/schedule/Schedule';
-import TestScores from './components/test-scores/TestScores';
-import loadOverview from './data-source';
+import TestResults from './components/test-results/TestResults';
 
+$(() => {
+  const uri = URI(window.location.href);
+  const queryDataMap = uri.search(true);
+  const studentsDcid = queryDataMap.students_frn.slice(3);
+  const yearId = queryDataMap.yearid;
 
-loadOverview()
-  .then(results => {
-    render(
-      <Router history={hashHistory}>
-        <Route path="/" component={Nav}>
-          <IndexRoute {...results} component={Overview} />
-          <Route path="/schedule" component={Schedule} />
-          <Route path="/test-results" component={TestScores} />
-        </Route>
-      </Router>,
-      document.getElementById('profile-container')
-    );
-  });
+  render(
+    <Router history={hashHistory}>
+      <Route path="/" component={Nav}>
+        <IndexRoute component={Overview} studentsDcid={studentsDcid} />
+        <Route path="/schedule" component={Schedule} studentsDcid={studentsDcid} yearId={yearId} />
+        <Route path="/test-results" component={TestResults} studentsDcid={studentsDcid} yearId={yearId} />
+      </Route>
+    </Router>,
+    document.getElementById('profile-container')
+  );
+});
