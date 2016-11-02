252b64b7aba895865e8c268d028ae2e59dcccd91
jonesnc <purkinje90@gmail.com>
2016-11-02 08:32:31 -0600

    Added display conditions for Medical info

--- src/scripts/student-profile/js/components/overview/Medical.js
+++ src/scripts/student-profile/js/components/overview/Medical.js
@@ -5,15 +5,21 @@ export default class Medical extends Component {
     const alertWarningClasses = 'alert alert-danger medical-alert';
     return (
       <div className={alertWarningClasses} role="alert">
-        <p>
-          <strong className="medical-label">Medical Alert</strong> {this.props.alert_medical}
-        </p>
-        <p>
-          <strong className="medical-label">Allergies</strong> {this.props.allergies}
-        </p>
-        <p>
-          <strong className="medical-label">Medical Considerations</strong> {this.props.medical_considerations}
-        </p>
+        {this.props.alert_medical &&
+          <p>
+            <strong className="medical-label">Medical Alert:</strong> {this.props.alert_medical}
+          </p>
+        }
+        {this.props.allergies &&
+          <p>
+            <strong className="medical-label">Allergies:</strong> {this.props.allergies}
+          </p>
+        }
+        {this.props.medical_considerations && this.props.alert_medical !== this.props.medical_considerations &&
+          <p>
+            <strong className="medical-label">Medical Considerations:</strong> {this.props.medical_considerations}
+          </p>
+        }
       </div>
     );
   }
